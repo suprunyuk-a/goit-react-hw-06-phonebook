737c695f7dd6dea17993d933ae19c627046ef8cf
suprunyuk-a <chaserenergy@gmail.com>
2022-12-13 21:48:24 +0200

    add work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Section from '../components/Section/Section';
+import Filter from './Filter/Filter';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Phonebook">
+        <ContactForm />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        <ContactList />
+      </Section>
+    </>
   );
 };
